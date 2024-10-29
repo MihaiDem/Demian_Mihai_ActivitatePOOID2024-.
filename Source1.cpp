83831bb89714dc485ed4df05218c9f10f820fd82
Mihai <Mihai@DESKTOP-SEVN55R>
2024-10-29 20:33:30 +0200

    test

---
+++ Source1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1, num2, sum;
+
+	// Asking for input
+	cout << "Enter two numbers: ";
+	cin >> num1 >> num2;
+
+	// Calculating sum
+	sum = num1 + num2;
+
+	// Displaying the result
+	cout << "The sum is: " << sum << endl;
+
+	return 0;
+}
